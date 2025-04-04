eaacde81d674dafb47b79b7723767b6461e46124
majorprojectktr <majorprojectktr@gmail.com>
2025-04-04 15:18:52 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(["/"]);
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  const isPublic = isPublicRoute(req);
-  const { userId } = await auth();
-
-  if (!isPublic) {
-    await auth.protect();
-  }
-
-  if (userId) {
-    // Redirect authenticated users from public route
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
-
-  // Allow access for unauthenticated users on public routes
-  return NextResponse.next();
-});
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    "/(api|trpc)(.*)",
+    '/(api|trpc)(.*)',
   ],
-};
+}
